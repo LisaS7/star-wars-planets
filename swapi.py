60845c2e874b3ea45ed17aba96000bcc244c9386
Lisa Linux <mclisa00@gmail.com>
2022-07-11 12:56:51 +0100

    Refactoring and starting on Planets class

--- swapi.py
+++ swapi.py
@@ -1,23 +1,79 @@
 import requests
 import requests_cache
+import configparser
 from datetime import timedelta
 
-API_URL = 'https://swapi.dev/api/planets/'
-CACHE_EXPIRE_AFTER = timedelta(days=30)
 
-requests_cache.install_cache(cache_name='planet_cache', backend='sqlite', expire_after=CACHE_EXPIRE_AFTER)
+# __________CONFIG__________
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 
-response = requests.get(API_URL).json()
-# print(response.json())
+# __________FUNCTIONS__________
+def get_page_json(url):
+    response = requests.get(url)
+    if response.ok:
+        return response.json()
+    else:
+        return None
 
-planet_list = response['results']
 
-print(planet_list)
+# __________CLASSES__________
+class BasicApi:
+    def __init__(self, API):
+        self.api = API
+        self.url = None
+        self.content = None
+        self.next_url = None
+        self.config()
+        self.create_cache()
 
-while response['next']:
-    response = requests.get(response['next']).json()
-    planet_list.append(response['results'])
+    def config(self):
+        try:
+            self.url = config.get(self.api, 'URL')
+            self.content = config.get(self.api, 'CONTENT_LABEL')
+            self.next_url = config.get(self.api, 'NEXT_URL_LABEL')
+        except configparser.NoSectionError:
+            print('API not defined in config.ini')
+        except Exception as e:
+            print(e)     
 
-for planet in planet_list[0:3]:
-    print(planet)
+    def create_cache(self):
+        cache_expiry_days = int(config.get(self.api, 'CACHE_EXPIRY_DAYS'))
+
+        requests_cache.install_cache(
+            cache_name=config.get(self.api, 'CACHE_NAME'),
+            backend='sqlite',
+            expire_after=timedelta(days=cache_expiry_days)
+        )
+
+    def get_all_pages(self):
+        response = get_page_json(self.url)
+        content_list = response[self.content]
+
+        if response[self.next_url]:
+            while response[self.next_url]:
+                response = get_page_json(response[self.next_url])
+                content_list.append(response[self.content])
+        else:
+            print('No next page URL found.')
+
+        return content_list
+
+
+class Planet:
+    def __init__(self, dict):
+        self.name = dict['name']
+
+
+if __name__ == '__main__':
+    SWAPI = BasicApi('SWAPI')
+
+    planet_data = SWAPI.get_all_pages()
+    planets = []
+
+    for planet in planet_data[0:5]:
+        print(planet)
+        planets.append(Planet(planet))
+        
+    print(planets)
