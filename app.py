0c12530aaed5430f513b1bda2424690ef68856da
Lisa Linux <mclisa00@gmail.com>
2022-07-14 10:35:52 +0100

    Added routes for climate and terrain.

--- app.py
+++ app.py
@@ -5,12 +5,24 @@ app = Flask(__name__)
 
 SWAPI = BasicApi('SWAPI')
 planet_data = SWAPI.get_all_pages()
-planets = create_planets(planet_data)
+all_planets = create_planets(planet_data)
 
 
 @app.route("/")
-def hello_world():
-    return render_template('index.html', planets=planets)
+def index():
+    return render_template('index.html', planets=all_planets)
+
+
+@app.route("/climate/<selected_climate>")
+def view_by_climate(selected_climate):
+    climate_planets = [planet for planet in all_planets if selected_climate in planet.climate]
+    return render_template('index.html', planets=climate_planets)
+
+
+@app.route("/terrain/<selected_terrain>")
+def view_by_terrain(selected_terrain):
+    terrain_planets = [planet for planet in all_planets if selected_terrain in planet.terrain]
+    return render_template('index.html', planets=terrain_planets)
 
 
 if __name__ == '__main__':
