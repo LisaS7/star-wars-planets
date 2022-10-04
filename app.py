26737716b6e8285c42f074dfbfe691a261abc1f2
forest <mclisa00@gmail.com>
2022-10-04 08:33:28 +0100

    Fixing urls for deploying

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from swapi.swapi import BasicApi, create_planets
+
 
 app = Flask(__name__, static_url_path="/star-wars-planets/static")
 
@@ -8,8 +9,9 @@ planet_data = SWAPI.get_all_pages()
 all_planets = create_planets(planet_data)
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def index():
+    print(request.headers)
     print(app.static_url_path)
     print(app.static_folder)
     return render_template('index.html', planets=all_planets)
@@ -17,13 +19,16 @@ def index():
 
 @app.route("/climate/<selected_climate>")
 def view_by_climate(selected_climate):
-    climate_planets = [planet for planet in all_planets if selected_climate in planet.climate]
+
+    climate_planets = [
+        planet for planet in all_planets if selected_climate in planet.climate]
     return render_template('index.html', planets=climate_planets)
 
 
 @app.route("/terrain/<selected_terrain>")
 def view_by_terrain(selected_terrain):
-    terrain_planets = [planet for planet in all_planets if selected_terrain in planet.terrain]
+    terrain_planets = [
+        planet for planet in all_planets if selected_terrain in planet.terrain]
     return render_template('index.html', planets=terrain_planets)
 
 
